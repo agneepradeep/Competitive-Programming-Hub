05f9796b72e57637f33c88eb52b7cbf84f283ccb
Agneepradeep Verma <agneepradeep31@gmail.com>
2024-10-03 21:37:24 +0530

    linear search

--- Arrays/Basic Problems/ArrayOperations.java
+++ Arrays/Basic Problems/ArrayOperations.java
@@ -49,4 +49,13 @@ public ArrayOperations {
             return b;
         }
     }
+
+    public int linearSearch(int[] arr, int item) {
+        for (int i = 0; i< arr.length; i++) {
+            if (arr[i] == item) {
+                return i;
+            } else continue;
+        }
+        return -1;
+    } 
 }
